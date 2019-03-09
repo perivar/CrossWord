28dac48faefdf1ae1a66fe2b3c7894515a545049
Per Ivar Nerseth <perivar@nerseth.com>
2019-03-09 11:20:47 +0100

    fixed paging

--- CrossWord.Scraper/Program.cs
+++ CrossWord.Scraper/Program.cs
@@ -55,26 +55,18 @@ namespace CrossWord.Scraper
                 var ready2 = wait2.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
             }
 
-            // try
-            // {
-            //     var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-            //     wait.Until(c => c.Url.Equals(""));
-            // }
-            // catch (WebDriverTimeoutException)
-            // {
-            //     Console.WriteLine("Timeout - Logged in to AliExpress to late. Stopping.");
-            //     return;
-            // }
-
-            // go to search list, first page
-            var query = "test";
+            // go to search result page
+            var letterCount = 2;
+            var query = "";
             int page = 0;
-
-            string url = string.Format("{0}?a={1}&b=&p={2}", "https://www.kryssord.org/search.php", query, page);
+            string url = string.Format("{0}?a={1}&b={2}&p={3}", "https://www.kryssord.org/search.php", query, letterCount, page);
             driver.Navigate().GoToUrl(url);
 
             while (true)
             {
+                // parse total number of words found
+                var wordCount = driver.FindElement(By.XPath("/html/body//div[@id='content']/h1/strong")).Text;
+
                 // parse all words
                 IWebElement tableElement = driver.FindElement(By.XPath("/html/body//div[@class='results']/table/tbody"));
                 IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
@@ -82,12 +74,15 @@ namespace CrossWord.Scraper
                 foreach (IWebElement row in tableRow)
                 {
                     rowTD = row.FindElements(By.TagName("td"));
-                    var w = rowTD[0].Text;
-                    Console.WriteLine(w);
+                    var word = rowTD[0];
+
+                    Console.WriteLine(word);
+
+                    GetWordSynonyms(word.Text, driver);
                 }
 
                 // go to next page if exist
-                var selectedPage = driver.FindElement(By.XPath("//div[@class='pages']/ul/li/a/span[@class='mobile-hide']"));
+                var selectedPage = FindElementOrNull(driver, By.XPath("//div[@class='pages']/ul/li/a/span[contains(., 'Neste')]"));
                 if (selectedPage != null)
                 {
                     selectedPage.Click();
@@ -99,28 +94,71 @@ namespace CrossWord.Scraper
             }
         }
 
-        static Tuple<int, int> GetPageNumber(IWebDriver driver)
+        static void GetWordSynonyms(string word, IWebDriver driver)
         {
-            int curPage = 0;
-            int numPages = 0;
-            // var pagesSection = driver.FindElement(By.CssSelector("div[class$='pages']"));
-            var selectedPage = driver.FindElement(By.CssSelector("li[class$='active']"));
+            // open a new tab and set the context
+            var chromeDriver = (ChromeDriver)driver;
 
-            // new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='pages']//ul//li[@class='active']//following::li[1]/a"))).Click();
+            // save a reference to our original tab's window handle
+            var originalTabInstance = chromeDriver.CurrentWindowHandle;
 
-            Regex regexObj = new Regex(@"(\d+)/(\d+)", RegexOptions.IgnoreCase);
-            Match matchResults = regexObj.Match(selectedPage.ToString());
-            if (matchResults.Success)
+            // execute some JavaScript to open a new window
+            chromeDriver.ExecuteScript("window.open();");
+
+            // save a reference to our new tab's window handle, this would be the last entry in the WindowHandles collection
+            var newTabInstance = chromeDriver.WindowHandles[driver.WindowHandles.Count - 1];
+
+            // switch our WebDriver to the new tab's window handle
+            chromeDriver.SwitchTo().Window(newTabInstance);
+
+            // lets navigate to a web site in our new tab
+            var letterCount = "";
+            var query = word;
+            int page = 0;
+            string url = string.Format("{0}?a={1}&b={2}&p={3}", "https://www.kryssord.org/search.php", query, letterCount, page);
+            driver.Navigate().GoToUrl(url);
+
+            while (true)
             {
-                curPage = int.Parse(matchResults.Groups[1].Value);
-                numPages = int.Parse(matchResults.Groups[2].Value);
-                return new Tuple<int, int>(curPage, numPages);
+                // parse total number of words found
+                var wordCount = driver.FindElement(By.XPath("/html/body//div[@id='content']/h1/strong")).Text;
+
+                // parse all words
+                IWebElement tableElement = driver.FindElement(By.XPath("/html/body//div[@class='results']/table/tbody"));
+                IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
+                IList<IWebElement> rowTD;
+                foreach (IWebElement row in tableRow)
+                {
+                    rowTD = row.FindElements(By.TagName("td"));
+                    var synonym = rowTD[0].Text;
+
+                    Console.WriteLine("{0} is a synonym for {1}", synonym, word);
+                }
+
+                // go to next page if exist
+                var selectedPage = FindElementOrNull(driver, By.XPath("//div[@class='pages']/ul/li/a/span[contains(., 'Neste')]"));
+                if (selectedPage != null)
+                {
+                    var nextAnchor = GetParent(selectedPage).GetAttribute("href");
+                    driver.Navigate().GoToUrl(nextAnchor);
+                }
+                else
+                {
+                    break;
+                }
             }
 
-            return null;
+            // now lets close our new tab
+            chromeDriver.ExecuteScript("window.close();");
+
+            // and switch our WebDriver back to the original tab's window handle
+            chromeDriver.SwitchTo().Window(originalTabInstance);
+
+            // and have our WebDriver focus on the main document in the page to send commands to 
+            chromeDriver.SwitchTo().DefaultContent();
         }
 
-        static bool IsElementPresent(IWebDriver driver, By by)
+        private static bool IsElementPresent(IWebDriver driver, By by)
         {
             try
             {
@@ -131,6 +169,24 @@ namespace CrossWord.Scraper
             {
                 return false;
             }
+        }
+
+        private static IWebElement FindElementOrNull(IWebDriver driver, By by)
+        {
+            try
+            {
+                var webElement = driver.FindElement(by);
+                return webElement;
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        private static IWebElement GetParent(IWebElement node)
+        {
+            return node.FindElement(By.XPath(".."));
         }
     }
 }
