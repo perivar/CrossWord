f8f95769e31e95fda1783c10de8dab4d295b723a
Per Ivar Nerseth <perivar@nerseth.com>
2019-03-22 02:17:50 +0100

    disable waiting timeout

--- CrossWord.Scraper/Program.cs
+++ CrossWord.Scraper/Program.cs
@@ -107,9 +107,9 @@ namespace CrossWord.Scraper
                 service.WhitelistedIPAddresses = "127.0.0.1";
                 // service.EnableVerboseLogging = true;
 
-                IWebDriver driver = new ChromeDriver(service, options, TimeSpan.FromSeconds(20));
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.Manage().Window.Maximize();
+                IWebDriver driver = new ChromeDriver(service, options, TimeSpan.FromSeconds(30));
+                // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30); // this make findelement throw a timeout error if it doesn't exist 
+                // driver.Manage().Window.Maximize();
 
                 Log.Information("Using chromedriver path: '{0}', options: {1}", chromeDriverPath, options);
 
