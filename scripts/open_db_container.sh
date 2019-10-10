8253ad60658e9ec5db378a6c0adb2297f971ec3e
perivar <perivar@nerseth.com>
2019-10-10 16:00:24 +0200

    Fixed container name

--- scripts/open_db_container.sh
+++ scripts/open_db_container.sh
@@ -4,5 +4,5 @@ clear
 
 echo "Entering the dictionary database"
 
-docker exec -it crosswordapi.db mysql -uroot -psecret
+docker exec -it crossword.db mysql -uroot -psecret
 #mysql -P 3360 --protocol=tcp -uroot -psecret
