083464401ac6d8befb7acce0179d484d95377626
Per Ivar Nerseth <perivar@nerseth.com>
2023-08-03 16:41:27 +0200

    Update Startup.cs to latest odata api example

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.OData;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OData.ModelBuilder;
 using System.Text.Json;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
 using TodoApi.Models;
 
 namespace TodoApi
@@ -19,25 +17,38 @@ namespace TodoApi
         public void ConfigureServices(IServiceCollection services)
         {
             var modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EnableLowerCamelCase(); // turn on "lower camel case" for the whole model 
             modelBuilder.EntityType<Order>();
             modelBuilder.EntitySet<Customer>("Customers");
 
+            // Add services to the container.
+            services.AddAuthorization();
+
             services.AddControllers()
-            // .AddJsonOptions(options =>
-            //     {
-            //         options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-            //         options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-            //     })
+            .AddJsonOptions(options =>
+                {
+                    // turn on camel case for non EDM controllers, see EnableLowerCamelCase() for EDM models
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                    // options.JsonSerializerOptions.WriteIndented = true; // disable indented to save bandwidth and align with odata defaults
+                })
             .AddOData(
                 options =>
                 {
-                    options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null);
+                    options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(300);
                     options.AddRouteComponents("odata", modelBuilder.GetEdmModel());
                 });
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
+            services.AddCors(option =>
+            {
+                option.AddPolicy("Everything",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                    );
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,12 +57,17 @@ namespace TodoApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-                app.UseSwagger();
-                app.UseSwaggerUI();
             }
 
+            // Use odata route debug, /$odata
+            app.UseODataRouteDebug();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
             // app.UseHttpsRedirection();
+
+            app.UseCors("Everything");
 
             app.UseRouting();
 
@@ -61,6 +77,19 @@ namespace TodoApi
             {
                 endpoints.MapControllers();
             });
+
+            /*
+                ASP.NET Core provides the Microsoft.AspNetCore.OpenApi package
+                to interact with OpenAPI specifications for endpoints. 
+                The package acts as a link between the OpenAPI models that are defined 
+                in the Microsoft.AspNetCore.OpenApi package and the 
+                endpoints that are defined in Minimal APIs. 
+                The package provides an API that examines an endpoint's parameters, 
+                responses, and metadata to construct an OpenAPI annotation 
+                type that is used to describe an endpoint. 
+                Calling .WithOpenApi() on the endpoint adds to the endpoint's metadata. T 
+                https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis/openapi?view=aspnetcore-7.0          
+            */
         }
     }
 }
