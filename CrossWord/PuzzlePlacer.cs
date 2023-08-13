2061e2ebcb0daeeea23497b690cb0f4665a16505
Per Ivar Nerseth <perivar@nerseth.com>
2023-08-13 13:56:02 +0200

    add IsPuzzle support

--- CrossWord/PuzzlePlacer.cs
+++ CrossWord/PuzzlePlacer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CrossWord;
@@ -20,9 +21,14 @@ class PuzzlePlacer
         var puzzle = NormalizePuzzle(_puzzle).AsMemory();
         var board = _board.Clone();
         board.Preprocess(dictionary);
+
         var patterns = new List<CrossPattern>();
         for (int i = 0; i < board.GetPatternCount(); i++)
+        {
             patterns.Add(board.GetCrossPattern(i));
+        }
+
+        // sort by word length
         patterns.Sort((a, b) => -1 * a.Length.CompareTo(b.Length));
         if (patterns.Count == 0)
             yield break;
@@ -37,17 +43,34 @@ class PuzzlePlacer
             for (; idx < patterns.Count; idx++)
             {
                 var pattern = patterns[idx];
-                if (restPuzzleLength < pattern.Length) continue;
-                if (restPuzzleLength - pattern.Length == 1) continue;
+
+                if (restPuzzleLength < pattern.Length)
+                {
+                    continue;
+                }
+
+                if (restPuzzleLength - pattern.Length == 1)
+                {
+                    break; // PIN: this was a continue statement - which seems like a bug
+                }
+
                 var trans = pattern.TryFillPuzzle(puzzle.Slice(puzzle.Length - restPuzzleLength,
                     pattern.Length).Span, dictionary);
+
                 if (trans != null)
                 {
                     trans.Transform(pattern);
                     if (restPuzzleLength == pattern.Length)
                     {
-                        var cloned = board.Clone();
+                        // ensure only one pattern is marked as a puzzle
+                        patterns.All(c => { c.IsPuzzle = false; return true; });
+
+                        // set the current pattern as puzzle
+                        pattern.IsPuzzle = true;
+
+                        var cloned = board.Clone();  // clone before we revert the puzzle pattern
                         trans.Undo(pattern);
+
                         yield return cloned;
                         continue;
                     }
@@ -62,7 +85,10 @@ class PuzzlePlacer
             }
 
             if (stack.Count == 0)
+            {
                 break;
+            }
+
             idx = stack.Pop();
             var appTr = appliedTransformations.Pop();
             appTr.Undo(appTr.Pattern);
@@ -76,7 +102,9 @@ class PuzzlePlacer
         foreach (var c in puzzle)
         {
             if (char.IsLetter(c))
+            {
                 builder.Append(char.ToUpper(c));
+            }
         }
 
         return builder.ToString();
